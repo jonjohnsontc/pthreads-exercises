ce2dd5fd73ce581bea7c6b16c2a7602da0b5f893
Jon Johnson <jonjohnsontc@live.com>
2024-08-03 10:43:34 -0700

    finishing ch1

--- errors.h
+++ errors.h
@@ -44,7 +44,7 @@
 #define errno_abort(text)                                                      \
   do {                                                                         \
     fprintf(stderr, "%s at \"%s\":%d:%s\n", text, __FILE__, __LINE__,          \
-            strerror(code));                                                   \
+            strerror(errno));                                                  \
     abort();                                                                   \
   } while (0)
 #endif
