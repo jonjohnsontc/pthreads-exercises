128764fb85c3bb15d8be5691014c3a77a8603b39
Jon Johnson <jonjohnsontc@live.com>
2024-08-04 14:56:31 -0700

    fixing three after referencing book version

--- 3/alarm_mutex.c
+++ 3/alarm_mutex.c
@@ -1,7 +1,5 @@
 #include "../errors.h"
 #include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 /*
@@ -53,7 +51,7 @@ void *alarm_thread(void *arg) {
       else
         sleep_time = alarm->time - now;
 #ifdef DEBUG
-      printf("[waiting: %ld(%ld)\"%s\"]\n", alarm->time, sleep_time,
+      printf("[waiting: %ld(%d)\"%s\"]\n", alarm->time, sleep_time,
              alarm->message);
 #endif
     }
@@ -65,7 +63,7 @@ void *alarm_thread(void *arg) {
       run if it has been readied by user input, without delaying
       message if there's no input.
     */
-    status = pthread_mutex_lock(&alarm_mutex);
+    status = pthread_mutex_unlock(&alarm_mutex);
     if (status != 0)
       err_abort(status, "Unlock mutex");
     if (sleep_time > 0)
@@ -107,7 +105,7 @@ int main(int argc, char *argv[]) {
       (%64[^\n]), consisting of up to 64 characters
       separated from the seconds by whitespace
     */
-    if (sscanf(line, "%d%64[^\n]", &alarm->seconds, alarm->message) < 2) {
+    if (sscanf(line, "%d %64[^\n]", &alarm->seconds, alarm->message) < 2) {
       fprintf(stderr, "Bad command\n");
       free(alarm);
     } else {
@@ -143,11 +141,11 @@ int main(int argc, char *argv[]) {
 #ifdef DEBUG
       printf("[list:");
       for (next = alarm_list; next != NULL; next = next->link)
-        printf("%ld(%d)[\"%s\"]", next->time, next->time - time(NULL),
+        printf("%ld(%ld)[\"%s\"]", next->time, next->time - time(NULL),
                next->message);
       printf("]\n");
 #endif
-      status = pthread_mutex_lock(&alarm_mutex);
+      status = pthread_mutex_unlock(&alarm_mutex);
       if (status != 0)
         err_abort(status, "Unlock mutex");
     }
--- 3/notes.md
+++ 3/notes.md
@@ -10,3 +10,5 @@
 A visual of a mutex guarding some data. Whenever a threads line is below the bold `time` line is when it has made a successful lock on the mutex.
 
 ![Example mutex operation](./imgs/3-2_mutex_operation.png)
+
+We can statically and dynamically initialize mutexes. Static initialized mutexes are available through the `PTHREAD_MUTEX_INITIALIZER`. Dynamically allocated mutexes should be initialized through `pthread_mutex_init`, or or some other means, it just can't be aforementioned PTHREAD_MUTEX_INITIALIZER macro.
